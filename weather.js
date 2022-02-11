f70a7a873cff0a50eb3c95676d6a1a7c695caacc
Adilet K <adiletk312@gmail.com>
2022-02-11 16:21:40 +0600

    add: "Storage service"

--- weather.js
+++ weather.js
@@ -1,22 +1,30 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js'
+import { printHelp, printSuccess, printError } from './services/log.service.js'
+import { saveKeyValue, getKeyValue } from './services/storage.service.js'
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Токен сохранён')
+  } catch (e) {
+    printError(e.message)
+  }
+}
 
 const initCLI = () => {
-	const args = getArgs(process.argv)
-	console.log(args)
-	printHelp()
-	if (args.h) {
+  const args = getArgs(process.argv)
+  if (args.h) {
+    printHelp()
+  }
 
-	}
+  if (args.s) {
 
-	if (args.s) {
+  }
 
-	}
-
-	if (args.t) {
-
-	}
+  if (args.t) {
+    return saveToken(args.t)
+  }
 }
 
 initCLI()
